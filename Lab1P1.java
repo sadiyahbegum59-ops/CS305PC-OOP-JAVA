93f43fd4f7ecc665d2b6e068797157d95e3e5dd6
sadiyahbegum59-ops <sadiyahbegum59@gmail.com>
2025-09-03 11:29:43 +0530

    Add files via upload

---
+++ Lab1P1.java
@@ -0,0 +1,91 @@
+class Lab1P1{
+  public static void main (String...args){
+    byte b = 115;
+    Byte bb = b; //autoboxing
+        //int x = 200;
+        //byte b = (byte)x;
+      
+     System.out.println(Byte.MAX_VALUE);
+     System.out.println(bb.MIN_VALUE);
+     System.out.println(bb.SIZE);
+     System.out.println(bb.TYPE);
+     System.out.println(bb.BYTES);
+     System.out.println("--------bytes---------");
+     
+     
+     
+     
+      short s = 200;
+    Short ss = s;//Autobxing
+      
+     System.out.println(Short.MAX_VALUE);
+     System.out.println(ss.MIN_VALUE);
+     System.out.println(ss.SIZE);
+     System.out.println(ss.TYPE);
+     System.out.println(ss.BYTES);
+      System.out.println("---------short---------");
+      
+      
+      int i = 200;
+          Integer ii = i;//autoboxing
+      
+     System.out.println(Integer.MAX_VALUE);
+     System.out.println(ii.MIN_VALUE);
+     System.out.println(ii.SIZE);
+     System.out.println(ii.TYPE);
+     System.out.println(ii.BYTES);
+      System.out.println("---------INTEGER---------");
+       
+        long l = 200;
+          Long ll = l;//autoboxing
+      
+     System.out.println(Long.MAX_VALUE);
+     System.out.println(ll.MIN_VALUE);
+     System.out.println(ll.SIZE);
+     System.out.println(ll.TYPE);
+     System.out.println(ll.BYTES);
+      System.out.println("---------LONG---------");
+      
+      
+      
+        float f= 200;
+          Float ff = f;//autoboxing
+      
+     System.out.println(Float.MAX_VALUE);
+     System.out.println(ff.MIN_VALUE);
+     System.out.println(ff.SIZE);
+     System.out.println(ff.TYPE);
+     System.out.println(ff.BYTES);
+      System.out.println("---------FLOAT---------");
+      
+      
+       double d = 200;
+          Double dd = d;//autoboxing
+      
+     System.out.println(Double.MAX_VALUE);
+     System.out.println(dd.MIN_VALUE);
+     System.out.println(dd.SIZE);
+     System.out.println(dd.TYPE);
+     System.out.println(dd.BYTES);
+      System.out.println("---------DOUBLE---------");
+      
+       boolean bo = true;
+          Boolean bbo = bo;
+          System.out.println(bbo.TYPE);
+          System.out.println("---------BOOLEAN---------");
+      
+      
+     
+      
+      
+      
+      
+      
+     
+     
+     
+     
+     
+     
+  }
+}
