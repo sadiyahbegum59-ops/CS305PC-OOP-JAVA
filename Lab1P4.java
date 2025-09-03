0f52873a75a99a2d66e1b8ae98af18defd706afb
sadiyahbegum59-ops <sadiyahbegum59@gmail.com>
2025-09-03 12:41:02 +0530

    Add files via upload

---
+++ Lab1P4.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+class Lab1P4{
+public static void main (String...args){
+Scanner sc=new Scanner(System.in);
+System.out.println("enter the number1");
+int a=sc.nextInt();
+System.out.println("enter the number 2");
+int b=sc.nextInt();
+var c = (a/b);
+System.out.println(c);
+  
+  }
+  }
