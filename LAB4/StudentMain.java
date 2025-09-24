1342e0c2ac7bece558c3490790973d9823a2ad74
sadiyahbegum59-ops <sadiyahbegum59@gmail.com>
2025-09-24 12:43:16 +0530

    Add files via upload

---
+++ LAB4/StudentMain.java
@@ -0,0 +1,29 @@
+package Student;
+import java.util.Scanner;
+import java.util.io *;
+
+	class StudentMain implements Operations{
+	public void register(Student std){
+	
+	
+	
+	
+	
+	
+	}
+	
+	
+	
+	
+	
+	
+	public static void main(String...args){
+	
+	System.out.println("======================\nWelcome to SMS\n=====================");
+	
+	
+	}
+	
+	
+	
+	}
