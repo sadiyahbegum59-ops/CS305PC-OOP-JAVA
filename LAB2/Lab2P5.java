627db19f61d8c987651fd5070e95b30186dfba43
sadiyahbegum59-ops <sadiyahbegum59@gmail.com>
2025-10-29 22:01:06 +0530

    Add prime number checking functionality
    Implement a program to check if a number is prime.

---
+++ LAB2/Lab2P5.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+class Lab2p5 {
+    public static void main(String... args) {
+
+        Scanner s = new Scanner(System.in);
+
+        int a = s.nextInt();
+        int count = 0;
+
+        if (a <= 1) {
+            System.out.println("Number is not prime");
+            return;
+        }
+
+        int i = 2;
+        while (i < a) {
+            if (a % i == 0) {
+                count++;
+                break;
+            }
+            i++;
+        }
+
+        if (count == 0)
+            System.out.println("Prime");
+        else
+            System.out.println("Number is not prime");
+    }
+}
